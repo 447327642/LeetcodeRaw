ffbaff314a9e6cb1520c4b230b61ba7b108ed05d
447327642 <447327642@users.noreply.github.com>
2016-08-06 20:19:08 +0800

    [172. Factorial Trailing Zeroes][Accepted]committed by LeetCode Extension

--- 172-Factorial-Trailing-Zeroes/solution.java
+++ 172-Factorial-Trailing-Zeroes/solution.java
@@ -2,13 +2,13 @@ public class Solution {
     public int trailingZeroes(int n) {
         int a = 0;
         int x = 5;
-        while (x <= n && x <= Integer.MAX_VALUE / 5) {
+        while (x <= n && x <= Integer.MAX_VALUE / 5) { // if (x <= Integer.MAX_VALUE), x may overflow and hence enter another loop
             a += n / x;
             x *= 5;
         }
-        if (x > Integer.MAX_VALUE / 5) {
+        if (x > Integer.MAX_VALUE / 5) { // last step, does not operate x *= 5 and enter the condition check
             a += n / x;
-        }
+        } // 
         return a;
     }
 }
