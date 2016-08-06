b504b28fcdac1b5bafc20bfbc8071f5d253e807a
447327642 <447327642@users.noreply.github.com>
2016-08-06 22:58:31 +0800

    [100. Same Tree][Accepted]committed by LeetCode Extension

---
+++ 100-Same-Tree/solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (q == null && p == null) {
+            return true;
+        }
+        if (q == null && p != null || p == null && q != null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        } if ((p.left == null && q.left != null || p.right == null && q.right != null )||( p.right != null && q.right == null || p.right != null && q.right == null)) {
+            return false;
+        } if (p.left == null && q.left == null && p.right == null && q.right == null && p.val == q.val) {
+            return true;
+        } 
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
